09b704c748a4b66aff16e7edd470d62453ab5709
Darius S <pdst@bite.lt>
2015-04-08 17:16:08 +0300

    Removed curl and used Buzz\Browser to get weather

--- src/WeatherBundle/Provider/OpenWeatherMapProvider.php
+++ src/WeatherBundle/Provider/OpenWeatherMapProvider.php
@@ -4,18 +4,17 @@ namespace WeatherBundle\Provider;
 
 use WeatherBundle\Provider\WeatherProviderInterface;
 use WeatherBundle\Object\Location;
+use Buzz\Browser;
 
 class OpenWeatherMapProvider implements WeatherProviderInterface
 {
     public function getWeatherByLocation(Location $location)
     {
-        $url = "http://api.openweathermap.org/data/2.5/weather?lat={$location->getLatitude()}&lon={$location->getLongitude()}&units=metric";
-        $c = curl_init($url);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        $responseJson = curl_exec($c);
-        curl_close($c);
-        $response = json_decode($responseJson);
+        $browser = new Browser();
+        $response = $browser->get("http://api.openweathermap.org/data/2.5/weather?lat={$location->getLatitude()}&lon={$location->getLongitude()}&units=metric");
+        $contentJson = $response->getContent();
+        $content = json_decode($contentJson);
 
-        return $response->main->temp;
+        return $content->main->temp;
     }
 }
