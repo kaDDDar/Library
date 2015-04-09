f274ae330c317b021337c6c9abc1d2d90da82516
Darius S <pdst@bite.lt>
2015-04-09 14:30:15 +0300

    Buzz registered as service and injected into provider

--- src/WeatherBundle/Provider/OpenWeatherMapProvider.php
+++ src/WeatherBundle/Provider/OpenWeatherMapProvider.php
@@ -2,7 +2,7 @@
 
 namespace WeatherBundle\Provider;
 
-use WeatherBundle\Object\Location;
+use WeatherBundle\Struct\Location;
 use Buzz\Browser;
 
 /**
@@ -13,6 +13,16 @@ use Buzz\Browser;
 class OpenWeatherMapProvider implements WeatherProviderInterface
 {
     /**
+     * Inject Buzz browser service
+     *
+     * @param Browser $browser
+     */
+    public function __construct(Browser $browser)
+    {
+        $this->browser = $browser;
+    }
+
+    /**
      * Gets weather data from OpenWeatherMap, by location, and returns temperature
      *
      * @param Location $location
@@ -20,8 +30,7 @@ class OpenWeatherMapProvider implements WeatherProviderInterface
      */
     public function getWeatherByLocation(Location $location)
     {
-        $browser = new Browser();
-        $response = $browser->get("http://api.openweathermap.org/data/2.5/weather?lat={$location->getLatitude()}&lon={$location->getLongitude()}&units=metric");
+        $response = $this->browser->get("http://api.openweathermap.org/data/2.5/weather?lat={$location->getLatitude()}&lon={$location->getLongitude()}&units=metric");
         $contentJson = $response->getContent();
         $content = json_decode($contentJson);
 
